7d6e30a5ebb0976fbcb993272f0bed57d80fb083
VNv5 <ysk.smp69@gmail.com>
2026-04-02 15:32:26 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,8 +57,10 @@ window.onload = () => {
     popupMessage.textContent = msg;
     popup.classList.add('show');
   }
+
   popupClose.addEventListener('click', () => popup.classList.remove('show'));
 
+  // Web-App Mode handling
   if (isWebApp) {
     webAppToggle.checked = true;
     webAppToggle.disabled = true;
@@ -67,44 +69,45 @@ window.onload = () => {
     });
   } else {
     webAppToggle.checked = false;
-    webAppToggle.disabled = true;
+    webAppToggle.disabled = true; // can't activate web-app on normal browser
   }
 
-  [cloakButton, autoCloakToggle].forEach(el => {
-    if (!el) return;
-    el.addEventListener('click', e => {
-      if (webAppToggle.checked) {
-        e.preventDefault();
-        if (el.type === 'checkbox') el.checked = false;
-        showPopup('This Setting Cannot Be Activated Due To Web-App Mode');
-      }
-    });
-  });
-
+  // --- Auto Cloak persistence ---
   if (autoCloakToggle) {
     autoCloakToggle.checked = localStorage.getItem('autoCloak') === 'true';
+
+    // Auto Cloak triggers immediately on page load
+    if (autoCloakToggle.checked) cloak();
+
     autoCloakToggle.addEventListener('change', () => {
-      if (!webAppToggle.checked) {
-        localStorage.setItem('autoCloak', autoCloakToggle.checked);
-      } else {
-        autoCloakToggle.checked = false;
-        showPopup('This Setting Cannot Be Activated Due To Web-App Mode');
-      }
+      localStorage.setItem('autoCloak', autoCloakToggle.checked);
+      if (autoCloakToggle.checked) cloak();
     });
   }
 
+  // Cloak button click
+  cloakButton?.addEventListener('click', () => {
+    cloak();
+  });
+
+  function cloak() {
+    // Open blank tab
+    const newTab = window.open('about:blank', '_blank');
+
+    if (newTab) {
+      // Redirect current tab to Google
+      window.location.href = 'https://www.google.com';
+    } else {
+      // Popup fallback if blocked
+      showPopup('Popup blocked! Please allow popups to activate Cloak.');
+    }
+  }
+
+  // Panic button remains for later
   if (panicButtonToggle) {
     panicButtonToggle.checked = localStorage.getItem('panicButton') === 'true';
     panicButtonToggle.addEventListener('change', () => {
       localStorage.setItem('panicButton', panicButtonToggle.checked);
     });
   }
-
-  cloakButton?.addEventListener('click', () => {
-    if (!webAppToggle.checked) {
-      console.log('Cloak Activated!');
-    } else {
-      showPopup('This Setting Cannot Be Activated Due To Web-App Mode');
-    }
-  });
 };
