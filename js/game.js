34292c505932a9a5eaf92500465839bb83a76a45
VNv5 <ysk.smp69@gmail.com>
2026-04-10 13:30:35 -0400

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,45 @@
+const frame = document.getElementById("gameFrame");
+const exitBtn = document.getElementById("exitFullscreenBtn");
+
+function refreshGame() {
+  frame.src = frame.src;
+}
+
+function isFullscreen() {
+  return document.body.classList.contains("embed-fullscreen");
+}
+
+function toggleFullscreen() {
+  document.body.classList.toggle("embed-fullscreen");
+  exitBtn.style.display = isFullscreen() ? "block" : "none";
+  document.activeElement.blur();
+}
+
+function exitFullscreenMode() {
+  document.body.classList.remove("embed-fullscreen");
+  exitBtn.style.display = "none";
+}
+
+function openDeletePopup() {
+  document.getElementById("deletePopup").classList.add("show");
+}
+
+function closeDeletePopup() {
+  document.getElementById("deletePopup").classList.remove("show");
+}
+
+function confirmDelete() {
+  closeDeletePopup();
+  try {
+    const win = frame.contentWindow;
+    win.localStorage.clear();
+    win.sessionStorage.clear();
+    win.indexedDB.databases().then(dbs => {
+      dbs.forEach(db => win.indexedDB.deleteDatabase(db.name));
+    });
+  } catch(e) {}
+  frame.src = "about:blank";
+  setTimeout(() => {
+    frame.src = frame.getAttribute("data-src");
+  }, 300);
+}
